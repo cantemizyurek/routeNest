7adb198bf0bcbc2696db77b6294141edf836b74d
Can Temizyurek <can@temizyurek.com>
2023-09-26 15:59:01 -0400

    init v0.1.0

---
+++ README.md
@@ -0,0 +1,63 @@
+# 🌐 routeNest
+
+🛠 Easily structure and manage your Express.js routes using a directory-based approach. 🚀
+
+## 📦 Installation
+
+```bash
+npm install routenest
+```
+
+## 🌟 Features
+
+📂 Automatic Express route generation based on directory structure.
+⚙️ Supports middlewares and RESTful methods.
+🌲 Clean and organized route management using a tree structure.
+
+## 📖 Usage
+
+1. Import the library:
+
+```javascript
+import routeNest from 'routenest'
+```
+
+2. Organize your routes in a directory (e.g., /api):
+
+```
+/api
+  /users
+    get.js
+    post.js
+    /:id
+      get.js
+      put.js
+```
+
+3. Initialize with your routes directory:
+
+```javascript
+const app = routeNest.initExpress('/api')
+
+app.listen(3000)
+```
+
+💡 By default, routeNest looks into the /api directory, but you can customize the directory path by passing it to initExpress.
+
+## 📘 API
+
+`initExpress(directoryPath: string)`
+
+🔍 Initializes the Express application with routes and middlewares defined in the provided directory.
+
+**Parameters:**
+
+- `directoryPath`: (Optional) The path to the directory containing your routes. Defaults to `/api`.
+
+**Returns:**
+
+- An initialized Express application.
+
+## 🤝 Contributing
+
+If you'd like to contribute, please fork the repository and make changes as you'd like. Pull requests are warmly welcome.
---
+++ src/index.ts
@@ -0,0 +1,109 @@
+import path from 'path'
+import fs from 'fs'
+import express, { Request, Response, NextFunction } from 'express'
+
+type ApiHandler = (req: Request, res: Response, next?: NextFunction) => void
+type HttpMethod = 'post' | 'get' | 'put' | 'delete'
+
+type Handlers = {
+  [method in HttpMethod]?: ApiHandler
+} & {
+  middlewares: ApiHandler[]
+}
+
+interface ApiTree {
+  handlers: Handlers
+  children: {
+    [key: string]: ApiTree
+  }
+}
+
+const METHODS: HttpMethod[] = ['post', 'get', 'put', 'delete']
+
+export function initExpress(defaultPath: string = '/api') {
+  const apiTree = createApiTree()
+  setTreeFromFolder(path.join(__dirname, defaultPath), apiTree)
+
+  const app = express()
+  setRouterFromTree(apiTree, app)
+  return app
+}
+
+function setTreeFromFolder(folderPath: string, branch: ApiTree) {
+  fs.readdirSync(folderPath).forEach(item => {
+    const itemPath = path.join(folderPath, item)
+    const itemStat = fs.statSync(itemPath)
+
+    if (itemStat.isDirectory()) {
+      addDirectoryToTree(item, itemPath, branch)
+    } else if (itemStat.isFile()) {
+      addFileToTree(item, itemPath, branch)
+    }
+  })
+}
+
+function addDirectoryToTree(name: string, path: string, branch: ApiTree) {
+  branch.children[name] = createApiTree()
+  setTreeFromFolder(path, branch.children[name])
+}
+
+function addFileToTree(name: string, path: string, branch: ApiTree) {
+  const handler = require(path).default as ApiHandler
+  const method = name.split('.')[0]
+
+  if (method.startsWith('_')) return
+
+  if (isHttpMethod(method)) {
+    branch.handlers[method] = handler
+  } else {
+    addMiddleware(method, handler, branch)
+  }
+}
+
+function addMiddleware(name: string, middleware: ApiHandler, branch: ApiTree) {
+  const middlewareId = branch.handlers.middlewares.length
+  const [id, ...nameParts] = name.slice(1).split('-')
+  name = nameParts.join('-')
+  const desiredId = !isNaN(Number(id)) ? Number(id) : middlewareId
+
+  if (branch.handlers.middlewares[desiredId]) {
+    throw new Error(`Middleware with id ${desiredId} already exists`)
+  }
+
+  branch.handlers.middlewares[desiredId] = middleware
+}
+
+function isHttpMethod(method: string): method is HttpMethod {
+  return METHODS.includes(method as HttpMethod)
+}
+
+function setRouterFromTree(tree: ApiTree, router: express.Router, path = '') {
+  const { handlers, children } = tree
+
+  METHODS.forEach(method => {
+    if (handlers[method] !== undefined) {
+      router[method](path, ...handlers.middlewares, handlers[method]!)
+    }
+  })
+
+  if (handlers.middlewares.length > 0) {
+    router.use(path, ...handlers.middlewares)
+  }
+
+  Object.entries(children).forEach(([child, childTree]) => {
+    const childPath =
+      child.startsWith('[') && child.endsWith(']')
+        ? `${path}/:${child.slice(1, -1)}`
+        : `${path}/${child}`
+    setRouterFromTree(childTree, router, childPath)
+  })
+}
+
+function createApiTree(): ApiTree {
+  return {
+    handlers: {
+      middlewares: [],
+    },
+    children: {},
+  }
+}
